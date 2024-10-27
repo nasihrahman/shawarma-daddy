bf992b7e2864100409514da3068dc5659dcc21af
nasihrahman <nasihrahman6@gmail.com>
2024-10-27 12:30:33 +0530

    hero-1

---
+++ app.js
@@ -0,0 +1,29 @@
+// const expres = require('express');
+// const app = express();
+// onst path = require('path');
+
+
+
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, '/views'))
+ 
+
+// app.use(express.static(path.join(__dirname, 'public')))
+
+const nav = document.getElementById('mainNavbar');
+
+if(data-bs-toggle="collapse")
+    
+
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+}); $(function () {
+    $(document).scroll(function () {
+        var $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+});
